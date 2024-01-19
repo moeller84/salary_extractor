c5127c8e3ee122467183f4d8ab73e627f42d975b
moeller84 <moeller84@gmail.com>
2024-01-19 22:09:16 +0100

    found optimal scraping way

--- testing.py
+++ testing.py
@@ -1,27 +1,38 @@
 # %%
-import time
-import os.path
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.spotrac.com/rankings/nba/cap-hit/2024/'
+data = {
+    'ajax': 'true',
+    'mobile': 'false'
+}
+
+soup = BeautifulSoup(requests.post(url, data=data).content, 'html.parser')
 
 # %%
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-# %%
 
-# Set path to chromedriver as per your configuration
-homedir = os.path.expanduser("~")
-webdriver_service = Service(f"{homedir}/projects/salary_extractor/chromedriver")
+# Parse the HTML content
+salary_list = [element.text.strip() for element in soup.find_all('span', class_="info")]
+player_name_list = [element.text for element in soup.find_all('h3')]
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
 
 # %%
-browser.get("https://www.spotrac.com/rankings/nba/cap-hit/2024/")
+import pandas as pd
+import timeit
+
+# Example DataFrame
+data = {'player_name': player_name_list,
+        'salary_cap_hit': salary_list}
+df = pd.DataFrame(data)
+
 # %%
-description = browser.find_element(By.NAME, "description").get_attribute("content")
-print(f"{description}")
+fantrax_df = pd.read_csv(
+    "FantraxPlayers_g2m7u071ljznr6zx.csv", 
+    delimiter=",", 
+    names=["hash_id", "player_id", "player_name","team", "position", "salary_cap_hit"], 
+    index_col=False
+    )
+
 # %%
